e9eb635db3a606aa8f1379f805e6301ebc42978a
Abraham <nencho_1@hotmail.com>
2020-12-07 13:25:23 +0100

    Finished form and we did sass file

--- app.js
+++ app.js
@@ -3,13 +3,26 @@ var exphbs  = require('express-handlebars');
  
 var app = express();
 
-const libros = require ('./data/libros.json');
+const libros = require('./data/libros.json');
+let user = require('./data/user.json');
 
 app.engine('handlebars', exphbs({ partialsDir: __dirname + '/views/partials/' }));
 app.set('view engine', 'handlebars');
  
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get('/listaLibros', function (req, res) {
-    res.render('home',{libro: libros});
+    res.render('home', {libro: libros, user: user.userName});
 });
 
-app.listen(3000);
+app.get('/', function (req, res) {
+    res.render('login');
+});
+
+app.post('/login', function (req, res) {
+    user.userName = req.body.userName;
+    res.redirect('/listaLibros');
+});
+
+app.listen(3001);
